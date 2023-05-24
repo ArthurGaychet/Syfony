e2abdd80782602c49fbcdca4cc173f405dc2417b
vravanel <vravanel@gmail.com>
2023-05-24 11:55:21 +0200

    workshop ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\NutRepository;
 use App\Repository\SquirrelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,9 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(SquirrelRepository $squirrelRepository): Response
+    public function index(SquirrelRepository $squirrelRepository, NutRepository $NutRepository): Response
     {
         $squirrels = $squirrelRepository->findAll();
-        return $this->render('home/index.html.twig', ['squirrels'=>$squirrels]);
+        $nuts = $NutRepository->findAll();
+        return $this->render('home/index.html.twig', ['squirrels'=>$squirrels, 'nuts' => $nuts]);
     }
 }
